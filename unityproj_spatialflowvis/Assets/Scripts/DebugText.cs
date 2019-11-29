e8dbdebeeeb8940dfc83fe326ba84b6afb881e97
natalielh <nlehuenen@gmail.com>
2019-11-28 23:46:17 -0500

    Debug text fix

---
+++ unityproj_spatialflowvis/Assets/Scripts/DebugText.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //need to import UnityEngine.UI to control the text displayed on the GUI
+
+public class DebugText : MonoBehaviour
+{
+
+    private float nextActionTime = 0.0f;
+    private float updateTime = 1f;
+
+    private float deltaTime = 0.0f;
+
+    public Text screenText;
+
+    void Start()
+    {
+        //screenText = GameObject.Find("Canvas/DebugText").GetComponent<Text>();
+        screenText.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            screenText.enabled = !screenText.enabled;
+        }
+
+        if (Time.time > nextActionTime)
+        {
+            nextActionTime += updateTime;
+            float fps = 1.0f / deltaTime;
+            screenText.text =
+                // \n puts 'enter' in the string
+                "PRESS F1 TO SHOW / HIDE THIS TEXT \n" +
+                "[SPATIAL FLOW VISUALIZATION]  \n" +
+                "[CREATED BY NATALIE LE HUENEN, contact@natalielehuenen.com]  \n" +
+                "VERSION: [ALPHA-INTERNAL] \n" +
+                "APPLICATION RUNNING ON: [" + Application.platform + "] \n" +
+                "SCREEN RESOLUTION: [" + Screen.currentResolution + "] \n" +
+                "FPS: [" + fps + "] \n" +
+                "\n" +
+                "[CONTROLS ARE AS FOLLOWS:] \n" +
+                "[Z] SHOW / HIDE X-PLANE \n" +
+                "[X] SHOW / HIDE Y-PLANE \n" +
+                "[C] SHOW / HIDE Z-PLANE \n" +
+                "[V] SHOW / HIDE POINT AXES \n" +
+                "[1] SHOW / HIDE DEMO 1 \n" +
+                "[2] SHOW / HIDE DEMO 2 \n"
+
+            ;
+
+        }
+
+
+    }
+}
--- unityproj_spatialflowvis/Assets/Scripts/SpawnGrid.cs
+++ unityproj_spatialflowvis/Assets/Scripts/SpawnGrid.cs
@@ -5,30 +5,56 @@ using UnityEngine;
 public class SpawnGrid : MonoBehaviour {
 
     public GameObject prefab;
-    public Material material;
+
+    public Material matX;
+    public Material matY;
+    public Material matZ;
+    public Material mat1;
+
     public float distance = 0.1f;
 
 	// Use this for initialization
 	void Start () {
         for (int i=0; i<21; i++) {
             for (int j=0; j<21; j++) {
-                GameObject point = Instantiate(
+
+                //POINT 1
+                GameObject point1 = Instantiate(
                     prefab,
-                    //new Vector3(
-                    //    (-10f * distance) + (i * distance),
-                    //    (-10f * distance) + (j * distance),
-                    //    0
-                    //),
-                    //Quaternion.identity,
                     this.gameObject.transform,
                     false
                 );
-                //this.transform.parent = transform;
-                point.transform.localPosition = new Vector3(
+                point1.transform.localPosition = new Vector3(
                         (1 / this.transform.localScale.x) * ((-10f * distance) + (i * distance)),
                         (1 / this.transform.localScale.y) * ((-10f * distance) + (j * distance)),
                         0
-                    );
+                );
+                //matX = GetComponent<Renderer>().material;
+                
+
+                //POINT 2
+                GameObject point2 = Instantiate(
+                    prefab,
+                    this.gameObject.transform,
+                    false
+                );
+                point2.transform.localPosition = new Vector3(
+                        0,
+                        (1 / this.transform.localScale.y) * ((-10f * distance) + (i * distance)),
+                        (1 / this.transform.localScale.z) * ((-10f * distance) + (j * distance))
+                );
+
+                //POINT 3
+                GameObject point3 = Instantiate(
+                    prefab,
+                    this.gameObject.transform,
+                    false
+                );
+                point3.transform.localPosition = new Vector3(
+                        (1 / this.transform.localScale.x) * ((-10f * distance) + (i * distance)),
+                        0,
+                        (1 / this.transform.localScale.z) * ((-10f * distance) + (j * distance))
+                );
 
             }
 
