cf1335d51b884ff74b39eb28c9bac75d671ca1ff
natalielh <nlehuenen@gmail.com>
2019-11-28 19:20:14 -0500

    Dot spawning (single plane) fixed

--- unityproj_spatialflowvis/Assets/Scripts/SpawnGrid.cs
+++ unityproj_spatialflowvis/Assets/Scripts/SpawnGrid.cs
@@ -4,14 +4,34 @@ using UnityEngine;
 
 public class SpawnGrid : MonoBehaviour {
 
-    public GameObject point;
+    public GameObject prefab;
     public Material material;
     public float distance = 0.1f;
 
 	// Use this for initialization
 	void Start () {
         for (int i=0; i<21; i++) {
-            Instantiate(point, new Vector3(0,1,2), Quaternion.identity, this.gameObject.transform);
+            for (int j=0; j<21; j++) {
+                GameObject point = Instantiate(
+                    prefab,
+                    //new Vector3(
+                    //    (-10f * distance) + (i * distance),
+                    //    (-10f * distance) + (j * distance),
+                    //    0
+                    //),
+                    //Quaternion.identity,
+                    this.gameObject.transform,
+                    false
+                );
+                //this.transform.parent = transform;
+                point.transform.localPosition = new Vector3(
+                        (1 / this.transform.localScale.x) * ((-10f * distance) + (i * distance)),
+                        (1 / this.transform.localScale.y) * ((-10f * distance) + (j * distance)),
+                        0
+                    );
+
+            }
+
         }
 	}
 	
