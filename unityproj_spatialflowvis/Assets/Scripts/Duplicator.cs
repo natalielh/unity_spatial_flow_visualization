48ae48e0c90fb6bd4e47a961123a3b479e2553fb
natalielh <nlehuenen@gmail.com>
2020-02-24 01:50:23 -0500

    basic city planner works

---
+++ unityproj_spatialflowvis/Assets/Scripts/Duplicator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Duplicator : MonoBehaviour {
+
+    public bool alwaysDupe = false;
+    bool dupedYet = false;
+
+    public GameObject objToSpawn;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Duplicate() {
+
+        if (!alwaysDupe && !dupedYet) {
+            Instantiate(objToSpawn, this.transform.position, Quaternion.identity);
+            dupedYet = true;
+        }
+
+    }
+}
