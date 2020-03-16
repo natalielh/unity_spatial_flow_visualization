aac3be494da8edd533137cc34a68b0c21d2f5a61
natalielh <nlehuenen@gmail.com>
2020-03-16 03:49:23 -0400

    hoverboard ctrl WIP
    skybox also rotates now

---
+++ unityproj_spatialflowvis/Assets/BtnImpl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BtnImpl : MonoBehaviour {
+
+    private Vector3 origPos = new Vector3();
+
+    public float offset = 0.05f;
+
+	// Use this for initialization
+	void Start () {
+        origPos = transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider collider) {
+
+        transform.Translate(Vector3.down * offset);
+    //    transform.localPosition.y = origPos.y - offset;
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        transform.localPosition = origPos;
+    }
+
+}
