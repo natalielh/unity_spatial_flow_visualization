18db1943bb187947b1b4a1363b3bce2a27234ef5
natalielh <nlehuenen@gmail.com>
2020-03-18 00:30:48 -0400

    City builder more advanced
    + Added dome over area
    + Dome and building materials changed to stencil shader materials
    + Added arrow bounce effect on snap zone hover
    + City grid is now generated using its script
    ! BUG: Laggy when there are many snap drop zones
    ! BUG: Pointer ends at snap drop zone colliders
    ! BUG: Cannot see building generators outside of the dome

---
+++ unityproj_spatialflowvis/Assets/Scripts/SpawnCityGrid.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCityGrid : MonoBehaviour {
+
+    public GameObject prefab;
+    public float distance = 0.1f;
+    public int num = 20;
+
+    // Use this for initialization
+    void Start()
+    {
+        SpawnGridPlane();
+
+    }
+
+
+    void SpawnGridPlane()
+    {
+
+        for (int i = 0; i < num+1; i++)
+        {
+            for (int j = 0; j < num+1; j++)
+            {
+                //PLANE X
+                GameObject point2 = Instantiate(
+                    prefab,
+                    gameObject.transform,
+                    false
+                );
+                point2.transform.localPosition = new Vector3(
+                        (1 / transform.localScale.x) * ((-num/2 * distance) + (i * distance)),
+                        0,
+                        (1 / transform.localScale.z) * ((-num/2 * distance) + (j * distance))
+                );
+
+            }
+
+        }
+
+    }
+
+
+
+}
