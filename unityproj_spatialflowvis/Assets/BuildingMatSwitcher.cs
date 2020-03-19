9d801cf51bedadd8e9aa460fbf796bf9e8b6d217
natalielh <nlehuenen@gmail.com>
2020-03-18 22:39:07 -0400

    Test obj changes colour on dome enter

---
+++ unityproj_spatialflowvis/Assets/BuildingMatSwitcher.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingMatSwitcher : MonoBehaviour {
+
+    public Material matToSwitchTo;
+
+    void Start()
+    {
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "DomeCollider") {
+
+            gameObject.GetComponent<Renderer>().material = matToSwitchTo;
+
+        }
+    }
+
+}
---
+++ unityproj_spatialflowvis/Assets/Scripts/SnapDropDistDisabler.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapDropDistDisabler : MonoBehaviour {
+
+    public float distanceThreshold = 1.3f;
+
+    private float nextActionTime = 0.0f;
+    private float updateTime = 1f;
+
+    private float deltaTime = 0.0f;
+
+    // Update is called once per frame
+    void Update () {
+
+        deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+
+        if (Time.time > nextActionTime) {
+
+            if (Vector3.Distance(transform.parent.position, transform.position) > distanceThreshold)
+            {
+                disableSnapDropZone();
+            }
+            else
+            {
+                enableSnapDropZone();
+            }
+        }
+	}
+
+    void disableSnapDropZone()
+    {
+        this.gameObject.transform.GetChild(0).gameObject.SetActive(false);
+    }
+
+    void enableSnapDropZone()
+    {
+        this.gameObject.transform.GetChild(0).gameObject.SetActive(true);
+    }
+
+}
