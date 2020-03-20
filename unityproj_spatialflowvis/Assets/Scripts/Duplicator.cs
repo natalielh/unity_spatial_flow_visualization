ca3cb61d68f890da4a4acc5a6364a208bd4d3a68
natalielh <nlehuenen@gmail.com>
2020-03-20 00:26:06 -0400

    Buildings stick to holo table properly when table is spun

--- unityproj_spatialflowvis/Assets/Scripts/Duplicator.cs
+++ unityproj_spatialflowvis/Assets/Scripts/Duplicator.cs
@@ -6,32 +6,41 @@ public class Duplicator : MonoBehaviour {
 
     public bool dupeItself = true;
     public bool alwaysDupe = false;
+    public bool reparent = true;
+
     bool dupedYet = false;
 
+    // objToSpawn: set a PREFAB, this will be used if dupeItself == false
     public GameObject objToSpawn;
+    // newParent: if == true, object will be reparented
+    public Transform newParent;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
     public void Duplicate() {
 
         if (!alwaysDupe && !dupedYet) {
 
+            // reference to the CLONE gameObject created
+            GameObject clone;
+
+            // if dupe itself set to 'no'
             if (!dupeItself)
             {
-                Instantiate(objToSpawn, this.transform.position, Quaternion.identity);
+                // then create the PREFAB in its place
+                clone = Instantiate(objToSpawn, this.transform.position, Quaternion.identity);
             }
+            // if dupe itself set to 'yes'
             else {
-                Instantiate(this.gameObject, this.transform.position, Quaternion.identity);
+                // then create ITSELF in its place
+                clone = Instantiate(this.gameObject, this.transform.position, Quaternion.identity);
             }
 
+            // set parent
+            if (reparent && (newParent != null)) {
+                clone.transform.SetParent(newParent);
+            }
+
+            // set the flag to TRUE to prevent THIS from being duped again and again and again
             dupedYet = true;
         }
 
