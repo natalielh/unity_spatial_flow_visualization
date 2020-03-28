f49f296bf85833279e6f4e5ae730ba2279033fb2
natalielh <nlehuenen@gmail.com>
2020-03-28 19:13:02 -0400

    Changes to acropolis

---
+++ unityproj_spatialflowvis/Assets/Scripts/NBuilding.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NBuilding : MonoBehaviour {
+
+    // This script handles the building node.
+    Vector3 output = new Vector3();
+
+    GameObject input1;
+    GameObject input2;
+    GameObject input3;
+
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (input1!=null && input2 != null && input1 != null) {
+            Output();
+        }
+
+	}
+
+    void Output() {
+
+    }
+}
