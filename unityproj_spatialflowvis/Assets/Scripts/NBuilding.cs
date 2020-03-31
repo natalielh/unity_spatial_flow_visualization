a9cfcadc7982b9f9cdeda54f2694e37c88f35978
natalielh <nlehuenen@gmail.com>
2020-03-30 21:23:46 -0400

    Create new scripts and tag for nodes

--- unityproj_spatialflowvis/Assets/Scripts/NBuilding.cs
+++ unityproj_spatialflowvis/Assets/Scripts/NBuilding.cs
@@ -7,19 +7,42 @@ public class NBuilding : MonoBehaviour {
     // This script handles the building node.
     Vector3 output = new Vector3();
 
-    GameObject input1;
-    GameObject input2;
-    GameObject input3;
+    GameObject connectedNode1;  //expect NNum
+    GameObject connectedNode2;  //expect NNum
+    GameObject connectedNode3;  //expect NNum
 
-	
-	// Update is called once per frame
-	void Update () {
+    float input1 = 0.0f;
+    float input2 = 0.0f;
+    float input3 = 0.0f;
 
-        if (input1!=null && input2 != null && input1 != null) {
-            Output();
+
+    // Update is called once per frame
+    void Update () {
+
+        //if (input1!=null && input2 != null && input1 != null) {
+        //    Output();
+        //}
+
+        // check connected node's OUTPUT type validity
+        if (connectedNode1 != null){
+            if (connectedNode1.tag == "NNum") {
+
+            }
         }
 
-	}
+        if (connectedNode2 != null) {
+
+        }
+
+        if (connectedNode3 != null) {
+
+        }
+
+        else {
+            // do not allow the invalid node to connect
+        }
+
+    }
 
     void Output() {
 
