0379cdd5fcf3fd8b95d8a70dc9a71377dbe56439
natalielh <nlehuenen@gmail.com>
2020-03-31 23:25:23 -0400

    Implement debug logging via events

---
+++ unityproj_spatialflowvis/Assets/Scripts/ConsoleLogger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsoleLogger : MonoBehaviour {
+
+    //private string text;
+
+    public void Log(string text) {
+        Debug.Log(text);
+    }
+}
